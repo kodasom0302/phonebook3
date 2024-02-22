1f19e7ee688ba23ffcae149a6f79ac6b22a83dbb
dasom <dasom589@gmail.com>
2024-02-22 13:08:26 +0900

    2024-02-22

--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -11,6 +11,70 @@ import com.javaex.vo.PersonVo;
 
 public class PhoneDao {
 	
+	public int personEdit(int no, String name, String hp, String company) {
+		int count = -1;
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 2. Connection 얻어오기
+			String url = "jdbc:mysql://localhost:3306/phone_db";
+			conn = DriverManager.getConnection(url, "phone", "phone");
+			
+			// 3. SQL문 준비 / 바인딩 / 실행
+				// - sql문 준비
+				String query = "";
+				query += " UPDATE person SET  name=?,"
+						+ "						hp=?,"
+						+ "						company=?"
+						+ "WHERE person_id=? ";
+				
+				//바인딩
+				pstmt = conn.prepareStatement(query);
+				pstmt.setString(1, name);
+				pstmt.setString(2, hp);
+				pstmt.setString(3, company);
+				pstmt.setInt(4, no);
+				
+			
+				//실행
+				count = pstmt.executeUpdate();
+
+			// 4.결과처리
+				System.out.println(count + "건 수정 되었습니다.");
+
+		} catch (ClassNotFoundException e) {
+		System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		} finally {
+		// 5. 자원정리
+		try {
+		if (rs != null) {
+		rs.close();
+		} 
+		if (pstmt != null) {
+		pstmt.close();
+		}
+		if (conn != null) {
+		conn.close();
+		}
+		} catch (SQLException e) {
+		System.out.println("error:" + e);
+		}
+		
+		}
+		
+		return count;
+		
+	}//edit
+	
 	public int personDelete(int no) {
 		
 		int count=-1;
